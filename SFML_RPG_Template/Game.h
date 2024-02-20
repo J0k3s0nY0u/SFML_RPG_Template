fdbcabdfeda65aa7e9dbd85c093c27edb7efb8d1
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-02-20 17:35:10 +0100

    Created State Class

--- SFML_RPG_Template/Game.h
+++ SFML_RPG_Template/Game.h
@@ -1,14 +1,6 @@
 #pragma once
  
-#include<iostream>
-#include<ctime>
-#include<cstdlib>
-
-#include"SFML/System.hpp"
-#include"SFML/Window.hpp"
-#include"SFML/Graphics.hpp"
-#include"SFML/Audio.hpp"
-#include"SFML/Network.hpp"
+#include "State.h"
 
 class Game
 {
