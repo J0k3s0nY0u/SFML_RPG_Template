512ce6777bb6c90350445ac14de4a00f0f9f39e8
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-02-27 20:59:57 +0100

    Debugged Keybinds

--- SFML_RPG_Template/Game.cpp
+++ SFML_RPG_Template/Game.cpp
@@ -32,11 +32,20 @@ void Game::initWindow()
 
 void Game::initKeys()
 {
-    this->supportedKeys["Escape"] = sf::Keyboard::Key::Escape;
-    this->supportedKeys["W"] = sf::Keyboard::Key::W;
-    this->supportedKeys["A"] = sf::Keyboard::Key::A;
-    this->supportedKeys["S"] = sf::Keyboard::Key::S;
-    this->supportedKeys["D"] = sf::Keyboard::Key::D;
+    std::ifstream ifs("Config/supported_keys.ini");
+
+    if (ifs.is_open())
+    {
+        std::string key = "";
+        int key_value = 0;
+        while (ifs >> key >> key_value)
+        {
+            this->supportedKeys[key] = key_value;
+        }
+    }
+
+    ifs.close();
+
     //DEBUG REMOVE LATER!
     for (auto i : this->supportedKeys)
     {
