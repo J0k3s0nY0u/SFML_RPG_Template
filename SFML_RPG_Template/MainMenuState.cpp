c1beeb8f3d7fe209946938191d7daf23c2f46977
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-06-12 14:26:55 +0100

    XCX

--- SFML_RPG_Template/MainMenuState.cpp
+++ SFML_RPG_Template/MainMenuState.cpp
@@ -75,7 +75,7 @@ void MainMenuState::updateInput(const float& dt)
 
 void MainMenuState::updateButtons()
 {
-	/*Updates all the buttons in this State and handles functionality*/
+	/* Updates all the buttons in this State and handles functionality */
 	for (auto &it : this->buttons)
 	{
 		it.second->update(this->mousePosView);
