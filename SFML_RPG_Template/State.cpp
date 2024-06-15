ffb7c55ca88160b5f2bc513b0b3fdb0379641940
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-06-15 21:45:18 +0100

    deleted check for quit debug code

--- SFML_RPG_Template/State.cpp
+++ SFML_RPG_Template/State.cpp
@@ -18,14 +18,6 @@ const bool& State::getQuit() const
 	return this->quit;
 }
 
-void State::checkForQuit()
-{
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->keybinds.at("CLOSE"))))
-	{
-		this->quit = true;
-	}
-}
-
 void State::updateMousePositions()
 {
 	this->mousePosScreen = sf::Mouse::getPosition();
--- SFML_RPG_Template/State.h
+++ SFML_RPG_Template/State.h
@@ -30,7 +30,7 @@ public:
 
 	const bool& getQuit() const;
 
-	virtual void checkForQuit();
+	virtual void checkForQuit() = 0;
 
 	virtual void endState() = 0;
 	virtual void updateMousePositions();
