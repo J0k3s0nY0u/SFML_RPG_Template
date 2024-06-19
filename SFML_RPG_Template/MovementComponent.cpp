f65b98a0a7ac2b4e8dd64e9e5ba8c1f610ddd3ea
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-06-19 21:37:20 +0100

    working on the movement physics

--- SFML_RPG_Template/MovementComponent.cpp
+++ SFML_RPG_Template/MovementComponent.cpp
@@ -19,15 +19,49 @@ const sf::Vector2f& MovementComponent::getVelocity() const
 //Functions
 void MovementComponent::move(const float dir_x, const float dir_y, const float& dt)
 {
+	/* Accelerating a sprite until it reaches its max velocity */
+
 	//Acceleration
 
-	this->velocity.x = this->maxVelocity * dir_x;
-	this->velocity.y = this->maxVelocity * dir_y;
+	this->velocity.x += this->acceleration * dir_x;
+
+	if (this->velocity.x > 0.f) // Check for Right
+	{
+		if (this->velocity.x > this->maxVelocity)
+		{
+			this->velocity.x = this->maxVelocity;
+		}
+	}
+	else if (this->velocity.x < 0.f) // Check for Left
+	{
+		if (this->velocity.x < -this->maxVelocity)
+		{
+			this->velocity.x = -this->maxVelocity;
+		}
+	}
+	
+	this->velocity.y += this->acceleration * dir_y;
 }
 
 void MovementComponent::update(const float& dt)
 {
 	//Decceleration
+	if (this->velocity.x > 0.f) // Check for Right
+	{
+		this->velocity.x -= decceleration;
+		if (this->velocity.x < 0.f)
+		{
+			this->velocity.x = 0.f;
+		}
+	}
+	else if (this->velocity.x < 0.f) // Check for Left
+	{
+		this->velocity.x += decceleration;
+		if (this->velocity.x > 0.f)
+		{
+			this->velocity.x = 0.f;
+		}
+	}
 
 	//Final Move
 	this->sprite.move(this->velocity * dt); //Uses Velocity
