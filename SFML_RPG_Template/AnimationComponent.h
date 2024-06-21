f17892cf88b50b1768b1ade23d17c7888b6f720b
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-06-21 19:54:17 +0100

    Created AnimationComponent Created Animation class within it

---
+++ SFML_RPG_Template/AnimationComponent.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class AnimationComponent
+{
+private:
+	class Animation
+	{
+	private:
+
+
+	public:
+
+	};
+
+public:
+	AnimationComponent();
+	virtual ~AnimationComponent();
+
+	//Functions
+
+};
+
