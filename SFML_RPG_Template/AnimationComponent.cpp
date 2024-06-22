7b6de5fb85fa160e4e33667b9832a1d538fefc6f
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-06-22 22:44:07 +0100

    WORKING ON THE ANIMATION COMPONENT

--- SFML_RPG_Template/AnimationComponent.cpp
+++ SFML_RPG_Template/AnimationComponent.cpp
@@ -7,16 +7,32 @@ AnimationComponent::AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_
 
 AnimationComponent::~AnimationComponent()
 {
+	for (auto &i : this->animations)
+	{
+		
+		delete i.second;
+	}
 }
 
 //Functions
 
 
-void AnimationComponent::addAnimation(const std::string key)
+void AnimationComponent::addAnimation(
+	const std::string key, 
+	sf::Sprite& sprite, sf::Texture& texture_sheet,
+	float animation_timer,
+	int start_frame_x, int start_frame_y, int frames_x, int frames_y, int width, int height
+)
 {
+	this->animations[key] = new Animation(
+		this->sprite, this->textureSheet,
+		animation_timer,
+		start_frame_x, start_frame_y, frames_x, frames_y, width, height
+	);
+
 }
 
 void AnimationComponent::play(const std::string key, const float& dt)
 {
-	this->animations[key].play(dt);
+	this->animations[key]->play(dt);
 }
--- SFML_RPG_Template/AnimationComponent.h
+++ SFML_RPG_Template/AnimationComponent.h
@@ -29,13 +29,13 @@ private:
 
 		Animation(sf::Sprite& sprite, sf::Texture& texture_sheet, 
 			float animation_timer, 
-			int start_x, int start_y, int end_x, int end_y, int width, int height)
+			int start_frame_x, int start_frame_y, int frames_x, int frames_y, int width, int height)
 			: sprite(sprite), textureSheet(texture_sheet), 
 			animationTimer(animation_timer), width(width), height(height)
 		{
-			this->startRect = sf::IntRect(start_x, start_y, width, height);
+			this->startRect = sf::IntRect(start_frame_x * width, start_frame_y * height, width, height);
 			this->currentRect = this->startRect;
-			this->endRect = sf::IntRect(end_x, end_y, width, height);
+			this->endRect = sf::IntRect(frames_x * width, frames_y * height, width, height);
 
 			this->sprite.setTexture(this->textureSheet, true);
 			this->sprite.setTextureRect(this->startRect);
@@ -76,14 +76,17 @@ private:
 
 	sf::Sprite& sprite;
 	sf::Texture& textureSheet;
-	std::map < std::string, Animation> animations;
+	std::map < std::string, Animation*> animations;
 
 public:
 	AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_sheet);
 	virtual ~AnimationComponent();
 
 	//Functions
-	void addAnimation(const std::string key);
+	void addAnimation(const std::string key, 
+		sf::Sprite& sprite, sf::Texture& texture_sheet,
+		float animation_timer,
+		int start_frame_x, int start_frame_y, int frames_x, int frames_y, int width, int height);
 
 	void play(const std::string key, const float& dt);
 };
