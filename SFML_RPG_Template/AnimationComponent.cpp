fd9dab61d707608b383abfdd0cf7a516b121dceb
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-06-22 17:00:37 +0100

    Still working on the animation component

--- SFML_RPG_Template/AnimationComponent.cpp
+++ SFML_RPG_Template/AnimationComponent.cpp
@@ -1,9 +1,17 @@
 #include "AnimationComponent.h"
 
-AnimationComponent::AnimationComponent()
+AnimationComponent::AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_sheet)
+	:sprite(sprite), textureSheet(texture_sheet)
 {
 }
 
 AnimationComponent::~AnimationComponent()
+{
+}
+
+//Functions
+
+
+void AnimationComponent::update(const float& dt)
 {
 }
